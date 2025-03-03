839e862e7f9c60f5b03d600c7f202c3ebb9661d6
Laurent Sun <laurent.sun@ibm.com>
2025-03-03 12:15:07 +0100

    change project directory

---
+++ RecipeApp/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bienvenue dans mon application React !</h1>
+      <p>C'est une application simple avec React.</p>
+    </div>
+  );
+}
+
+export default App;
