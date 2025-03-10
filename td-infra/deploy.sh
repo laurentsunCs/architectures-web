3ba431b771d86063e3001601012c5afdf1fa9f12
EwenQuim <ewen.quimerch@gmail.com>
2025-03-10 10:39:33 +0100

    Portainer + remove unused services

--- td-infra/deploy.sh
+++ td-infra/deploy.sh
@@ -4,4 +4,4 @@ echo "Deploying!"
 
  sshpass -p "$PASS" scp -r ./src/* santorini:~/
 
- sshpass -p "$PASS" ssh santorini "docker compose pull && docker compose up -d && docker image prune -f && docker compose logs -f"
+ sshpass -p "$PASS" ssh santorini "docker compose pull && docker compose up -d --remove-orphans && docker image prune -f && docker compose logs -f"
